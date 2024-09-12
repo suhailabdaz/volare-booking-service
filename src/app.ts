25c062fabedf2dbcaf2d65dc643a14f96ea4c6a5
suhailabdaz <suhail.abdaz@gmail.com>
2024-09-12 13:03:47 +0530

    express

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
 import { connectDB } from "./config/mongoDB.config";
 import RabbitMQClient from "./rabbitMQ/client"
+import express from "express";
+import { Application } from "express";
+import "dotenv/config";
+
 
 
 
 class App{
+  public app: Application;
+
   constructor(){
+    this.app=express()
+    this.app.listen(process.env.BOOKING_PORT,()=>{
+      console.log(`server  http://localhost:${process.env.BOOKING_PORT}`);
+      
+    })
       connectDB()
       RabbitMQClient.initialize()
   }
